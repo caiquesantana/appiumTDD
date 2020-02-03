6bf11866f7e4d6b723acb57c7348519a309476ef
caiquesantana <caiquesantana9@hotmail.com>
2020-02-03 13:37:21 -0300

    Foi implementado a massa de dados

---
+++ src/main/java/br/com/rsinet/hub_tdd/appium/utility/Constante.java
@@ -0,0 +1,8 @@
+package br.com.rsinet.hub_tdd.appium.utility;
+
+public class Constante {
+	public static final String URL = "http://127.0.0.1:4723/wd/hub";
+	public static final String Path_TestData = "C:\\Users\\caique.oliveira\\eclipse-workspace\\AppiumTDD\\src\\main\\java\\br\\com\\rsinet\\hub_tdd\\appium\\testData\\";
+	public static final String File_TestData = "TestData.xlsx";
+
+}
---
+++ src/main/java/br/com/rsinet/hub_tdd/appium/utility/ExcelDados.java
@@ -0,0 +1,5 @@
+package br.com.rsinet.hub_tdd.appium.utility;
+
+public class ExcelDados {
+
+}
